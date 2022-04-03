fa2b7c61edc4dcd3b438d9ded9b269383438a82b
Théo Gillet <gillettheo1@gmail.com>
2022-04-03 21:08:44 +0200

    print data in dom

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+const descriptionTown = document.querySelector('.description-ville')
 const formSearch = document.querySelector('form')
 const searchCP = document.querySelector('#searchCP')
 const informationCity = document.querySelector('.information-city')
@@ -6,52 +7,62 @@ let codePostal
 let statusCodePostal
 formSearch.addEventListener('submit', (e) => {
     e.preventDefault()
-    codePostal = searchCP.value
+    const divContainer = document.querySelectorAll('.divContainer')
+    divContainer.forEach(item => {
+        descriptionTown.removeChild(item)
+    });
+    codePostal = parseInt(searchCP.value)
     console.log(codePostal)
+    let codePostalString = String(codePostal)
 
-    if(codePostal.length >= 5){
-        fetch('https://happyapi.fr/api/codePostaux?codePostal=' + codePostal)
+    if(codePostalString.length == 5){
+        fetch('https://happyapi.fr/api/codePostaux?codePostal=' + codePostalString)
         .then(response => response.json())
         .then(data => {
             statusCodePostal = data.happyApiStatus
             console.log(statusCodePostal);
             if(statusCodePostal == '200 OK'){
-                fetch("https://apicarto.ign.fr/api/codes-postaux/communes/" + codePostal)
+                fetch("https://apicarto.ign.fr/api/codes-postaux/communes/" + codePostalString)
                 .then(response => response.json())
                 .then(data => {
                     console.log(data)
-                    console.log(data[0].codeCommune);
-                    
+                    console.log('commune 1 : ', data[0]);
+                    informationCity.textContent = 'Code postal trouvé'
+                    data.forEach( item => {
+                        let town = item.libelleAcheminement
+                        let codeTown = item.codeCommune
+                        let postalCode = item.codePostal
+                        createElement(codeTown, postalCode, town)
+                    });
                 })
             
             }else if(statusCodePostal == '404 Not Found'){
-                document.body.style.backgroundColor = 'red'
+                informationCity.textContent = 'Code postal inconnu'
             }
         })
-        
-        
+    }else{
+        informationCity.textContent = 'Aucun code postal renseigné'
     }
 })
 
-function createElement(codeCommune, codePostal, commune){
+function createElement(codeTown, postalCode, town){
     const divContainer = document.createElement('div')
     divContainer.classList.add('divContainer')
+
+    const townlItem = document.createElement('p')
+    townlItem.classList.add('townlItem')
+    townlItem.textContent = town
     
-    const codePostalItem = document.createElement('p')
-    codePostalItem.classList.add('codePostalItem')
-    codePostalItem.textContent = codePostal
+    const postalCodeItem = document.createElement('p')
+    postalCodeItem.classList.add('postalCodeItem')
+    postalCodeItem.textContent = postalCode
     
-    const codeCommuneItem = document.createElement('p')
-    codeCommuneItem.classList.add('codeCommunelItem')
-    codeCommuneItem.textContent = codeCommune
+    const codeTownItem = document.createElement('p')
+    codeTownItem.classList.add('codeTownlItem')
+    codeTownItem.textContent = codeTown
     
-    const communelItem = document.createElement('p')
-    communelItem.classList.add('communelItem')
-    communelItem.textContent = commune
     
-    divContainer.append(codePostalItem, codeCommuneItem, communelItem)
+    
+    divContainer.append(townlItem, postalCodeItem, codeTownItem)
+    descriptionTown.append(divContainer)
 }
-
-
-
-const codePostalItem = document.createElement
