f73a648f52f259f980339c6a3e8432eda28e82cd
Théo Gillet <gillettheo1@gmail.com>
2022-04-03 19:03:01 +0200

    add verification of postal code

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,36 @@
 const formSearch = document.querySelector('form')
 const searchCP = document.querySelector('#searchCP')
+const informationCity = document.querySelector('.information-city')
+console.log(informationCity);
 let codePostal
+let statusCodePostal
 formSearch.addEventListener('submit', (e) => {
     e.preventDefault()
     codePostal = searchCP.value
     console.log(codePostal)
-        fetch("https://apicarto.ign.fr/api/codes-postaux/communes/" + codePostal)
+
+    if(codePostal.length >= 5){
+        fetch('https://happyapi.fr/api/codePostaux?codePostal=' + codePostal)
         .then(response => response.json())
         .then(data => {
-            console.log(data[0].codeCommune);
-        }) 
+            statusCodePostal = data.happyApiStatus
+            console.log(statusCodePostal);
+            if(statusCodePostal == '200 OK'){
+                fetch("https://apicarto.ign.fr/api/codes-postaux/communes/" + codePostal)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    console.log(data[0].codeCommune);
+                    
+                })
+            
+            }else if(statusCodePostal == '404 Not Found'){
+                document.body.style.backgroundColor = 'red'
+            }
+        })
+        
+        
+    }
 })
 
 function createElement(codeCommune, codePostal, commune){
